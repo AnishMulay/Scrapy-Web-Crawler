6d2ef9cf9db7e73e5bf440e6cc262cd1b335eaaf
AnishMulay <anishm7030@gmail.com>
2021-05-18 20:45:55 +0530

    scraped data can now be stored in files

--- crawler/crawler/items.py
+++ crawler/crawler/items.py
@@ -9,4 +9,7 @@ import scrapy
 class CrawlerItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    title = scrapy.Field()
+    author = scrapy.Field()
+    tags = scrapy.Field()
     pass
