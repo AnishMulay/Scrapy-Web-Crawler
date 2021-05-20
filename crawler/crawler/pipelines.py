1d1899c9d29a3e6036f1afc65c1e1bd98478395c
AnishMulay <anishm7030@gmail.com>
2021-05-20 11:19:30 +0530

    the crawler can now follow links to next pages

--- crawler/crawler/pipelines.py
+++ crawler/crawler/pipelines.py
@@ -6,8 +6,36 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import sqlite3
 
 class CrawlerPipeline:
-    def process_item(self, item, spider):
-        return item
+	def __init__(self):
+		self.create_connection()
+		self.create_table()
+
+	def create_connection(self):
+		self.con = sqlite3.connect("scrapedquotes.db")
+		self.cur = self.con.cursor()
+
+	def create_table(self):
+		self.cur.execute("""drop table if exists quotes""")
+
+		self.cur.execute("""create table quotes(
+							title text,
+							author text,
+							tag text
+							)""")
+
+	def store(self, item):
+		self.cur.execute("""insert into quotes values (?, ?, ?)""",
+		(
+			item['title'][0],
+			item['author'][0],
+			item['tags'][0]
+		))
+		self.con.commit()
+	
+	def process_item(self, item, spider):
+		self.store(item)
+		print("pipeline: " + item['title'][0])
+		return item
